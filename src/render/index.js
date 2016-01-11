37d5223eb8662eb8cc2ba8643ee19de64d9e9a37
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-01-11 02:44:12 -0600

    Modularize :octopus:

---
+++ src/render/index.js
@@ -0,0 +1,31 @@
+/*
+ * Module dependencies
+ */
+import $ from 'jquery'
+import $tvShowsContainer from 'src/tv-shows-container'
+
+var template = `<article class="tv-show">
+                <div class="left img-container">
+                <img src=":img:" alt=":img alt:">
+                </div>
+                <div class="right info">
+                <h1>:name:</h1>
+                <p>:summary:</p>
+                <button class="like">💚</button>
+                </div>
+                </article>;`
+
+export default function renderShows(shows) {
+  $tvShowsContainer.find('.loader').remove();
+  shows.forEach(function(show) {
+    var article = template
+      .replace(':name:', show.name)
+      .replace(':summary:', show.summary)
+      .replace(':img:', show.image.medium)
+      .replace(':img alt:', show.name + " Logo");
+
+    var $article = $(article);
+    $article.hide();
+    $tvShowsContainer.append($article.fadeIn(1500));
+  });
+}
